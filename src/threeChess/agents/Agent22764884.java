db29465bb5a5e6def6372686593cf6478afa09b9
brownpanda <56075784+brownpanda1@users.noreply.github.com>
2020-10-05 13:45:16 +0800

    Update Agent22764884.java
    update

--- src/threeChess/agents/Agent22764884.java
+++ src/threeChess/agents/Agent22764884.java
@@ -1,46 +1,46 @@
 package threeChess.agents;
 
 import threeChess.*;
-import java.util.Random;
+import java.util.ArrayList;
 
 
 
 public class Agent22764884 extends Agent{
 	private static final String name = "22764884";
-	private static final Random random = new Random();
 	
 	
 	public Agent22764884() {}
+
+	public Position[][] nextMoves(Board board) {
+	// Find all of our piece positions and all of the board nextPositions
+    Position[] pieces = board.getPositions(board.getTurn()).toArray(new Position[0]);
+    Position[] next = Position.values();
+    ArrayList<Position[]> nextMoves = new ArrayList<>();
+    // Enumerate over all possible move nextPositions for all pieces
+    for (Position piece : pieces) {
+    	for (Position positions : next) {
+    		// Move piece to position
+    		Position[] move = new Position[] {piece, positions};
+        	if (board.isLegalMove(piece, positions) && !nextMoves.contains(move)) nextMoves.add(move);
+      		}
+    }
+    
+	return nextMoves.toArray(new Position[0][0]);
+	}
 	
 	@Override
 	public Position[] playMove(Board board) {
-		int moveCount = board.getMoveCount();
-		if(moveCount > 0) {
-			Position[] lastMove = board.getMove(moveCount-1);
-			try {
-				Position start = Position.get(Colour.values()[(lastMove[0].getColour().ordinal()+1)%3], lastMove[0].getRow(), lastMove[0].getColumn());
-				Position end = Position.get(Colour.values()[(lastMove[0].getColour().ordinal()+1)%3], lastMove[1].getRow(), lastMove[1].getColumn());
-				if(board.isLegalMove(start, end))return new Position[] {start, end};
-			}
-			catch(ImpossiblePositionException e) {}
-		}
-		Position[] pieces = board.getPositions(board.getTurn()).toArray(new Position[0]);
-		Position start = pieces[0];
-	    Position end = pieces[0]; //dummy illegal move
-	    while (!board.isLegalMove(start, end)){
-	      start = pieces[random.nextInt(pieces.length)];
-	      Piece mover = board.getPiece(start);
-	      Direction[][] steps = mover.getType().getSteps();
-	      Direction[] step = steps[random.nextInt(steps.length)];
-	      int reps = 1 + random.nextInt(mover.getType().getStepReps());
-	      end = start;
-	      try{
-	        for(int i = 0; i<reps; i++)
-	          end = board.step(mover, step, end, start.getColour()!=end.getColour());
-	      }catch(ImpossiblePositionException e){}
-	    }
-	    return new Position[] {start,end};
-	  }
+		
+		//Copy of current board
+		Board copy = board;
+		
+		//Find all possible next moves
+		Position[][] next = nextMoves(copy);
+		
+		//Perform MCTS
+		
+		
+	}
 	
 	/**
 	 * @return the Agent's name, for annotating game description.
@@ -49,5 +49,4 @@ public class Agent22764884 extends Agent{
 
 	@Override
 	public void finalBoard(Board finalBoard) {}
-
 }
